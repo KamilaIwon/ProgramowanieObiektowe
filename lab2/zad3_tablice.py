e771975b6b24cafeb49ead5b5be40d69d3c5e6e1
KamilaIwon <98175223+KamilaIwon@users.noreply.github.com>
2022-10-11 20:02:56 +0200

    lab2

--- lab2/zad3_tablice.py
+++ lab2/zad3_tablice.py
@@ -19,3 +19,14 @@ while len(wynik) < 10:
     wynik.append(list(range(1,len(wynik)+2)))
 
 print(wynik)
+
+tab = []
+a = 1
+for i in range(1,10):
+    row = []
+    for j in range(1,i):
+        row.append(a)
+        a += 1
+    tab.append(row)
+
+print(tab)
