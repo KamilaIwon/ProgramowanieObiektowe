4ef36307ffd4d7ad2f3e250776e061e18dab3a1e
KamilaIwon <iwon.kamila@gmail.com>
2022-12-01 01:41:54 +0100

    upp

--- przykladowe_kolo/Court1.py
+++ przykladowe_kolo/Court1.py
@@ -6,12 +6,16 @@ class Court:
     __year_built: int
 
     def __init__(self, address: str, year_built: int, width: float = 68, length: float = 150) -> None:
-        if 45 <= width <= 90 and 90 <= length <= 120:
+        if year_built < 2008:
+            if 45 <= width <= 90 and 90 <= length <= 120:
+                self.__width = width
+                self.__length = length
+            else:
+                self.__width = 68
+                self.__length = 150
+        else:
             self.__width = width
             self.__length = length
-        else:
-            self.__width = 68
-            self.__length = 150
         self.__address = address
         self.__year_built = year_built
 
@@ -21,9 +25,19 @@ class Court:
                f'address: {self.__address}\n' \
                f'year built: {self.__year_built}'
 
+    @property
+    def width(self) -> float:
+        return self.__width
+
+    @width.setter
+    def width(self, value: float) -> None:
+        self.__width = value
+
     def area(self) -> float:
         return self.__width * self.__length
 
-zmienna1 = Court('lotnicza', 2006, 190, 200)
+
+zmienna1 = Court('lotnicza', 2009, 190, 200)
 zmienna2 = Court('apparel', 2014, 70, 115)
-print(zmienna2.area())
+
+print(zmienna1)
